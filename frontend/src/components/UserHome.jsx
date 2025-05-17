a8d4b3e2873786127d59b9ef5cc7daadcd2b00ea
dinraj910 <dinrajdinesh564@gmail.com>
2025-05-17 19:59:47 +0530

    Final

--- frontend/src/components/UserHome.jsx
+++ frontend/src/components/UserHome.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "../UserHome.css";
+import "../userHome.css";
 
 function UserHome() {
   const navigate = useNavigate();
